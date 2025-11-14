4ea806e5c67c19a7c1be7b178bf2875761d5c192
baymeric <aymeric.beaune@ynov.com>
2025-11-14 10:15:33 +0100

    [ :memo: ] - Ajout couleur verte sur /color

--- main.go
+++ main.go
@@ -5,8 +5,15 @@ import (
 	"net/http"
 )
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
 func main() {
 	// to add : url functionality
 	// to add : color functionality
 	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/color", ColorHandler)
 }
+
+
